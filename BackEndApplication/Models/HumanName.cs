8cd24bdab1cf9aae8b60ce9a78fa92b70f2b16b9
Anatoly <you@example.com>
2019-09-13 02:50:01 +0300

    Changed adaptivity and name show.

--- BackEndApplication/Models/HumanName.cs
+++ BackEndApplication/Models/HumanName.cs
@@ -2,8 +2,9 @@
 {
     public class HumanName
     {
-        public string Use { get; set; }
+        public string Use { get; set; } // usual | official | temp | nickname | anonymous | old | maiden
         public string Text { get; set; }
+        public string Family { get; set; }
         public string[] Given { get; set; }
         public string[] Prefix { get; set; }
         public string[] Suffix { get; set; }
